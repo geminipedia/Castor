e58f441f355fb6117205f32a3c8139b001906e19
Aries Cs <aries0d0f@gmail.com>
2019-08-20 13:05:18 +0800

    [Layout] Add `Home` components.

---
+++ components/theme/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <section />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  head () {
+    return {
+      title: this.$route.meta.head.title,
+      meta: this.$route.meta.head.meta
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
