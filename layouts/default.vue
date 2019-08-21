6566a237c666802eb84a1570df0e9aecc3ac1b4b
Aries Cs <aries0d0f@gmail.com>
2019-08-21 21:25:59 +0800

    [Method] Poke API to verify user during loading.

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,11 +6,25 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import Navbar from '~/components/element/Navbar'
+
 export default {
   name: 'Default',
+
   components: {
     Navbar
+  },
+
+  async mounted () {
+    // Don't poke API when development at localhost
+    if (!window.location.host.includes('localhost')) {
+      await this.verifyUser()
+    }
+  },
+
+  methods: {
+    ...mapActions({ verifyUser: 'user/verify' })
   }
 }
 </script>
