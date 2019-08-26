5fbcddcf4fc57c16fb2eb6bb0cf3e1b7f97bf3f2
Aries Cs <aries0d0f@gmail.com>
2019-08-26 19:33:12 +0800

    [Method] Add `item` search method.

--- store/item.js
+++ store/item.js
@@ -3,12 +3,24 @@ import * as types from './mutation-types'
 
 export const state = () => ({
   items: [],
-  nowPage: 1
+  nowPage: 1,
+  search: {
+    orderBy: 'itemId_ASC',
+    rawText: '',
+    resultMode: false,
+    mode: 'NORMAL', // NORMAL | ADVANCED | QUERY
+    result: []
+  }
 })
 
 export const getters = {
-  items: state => state.items,
-  nowPage: state => state.nowPage
+  items: state => state.search.resultMode ? state.search.result : state.items,
+  nowPage: state => state.nowPage,
+  searchOrderBy: state => state.search.orderBy,
+  searchRawText: state => state.search.rawText,
+  searchResultMode: state => state.search.resultMode,
+  advanceSearch: state => state.search.mode === 'ADVANCE',
+  querySearch: state => state.search.mode === 'QUERY'
 }
 
 export const actions = {
@@ -183,8 +195,93 @@ export const actions = {
     }
   },
 
+  async search ({ commit }, { where, orderBy }) {
+    try {
+      const apollo = this.app.apolloProvider.defaultClient
+      const res = await apollo.query({
+        query: gql`
+          query($itemWhereInput: ItemWhereInput, $itemOrderBy: ItemOrderByInput) {
+            items(where: $itemWhereInput, orderBy: $itemOrderBy) {
+              id
+              itemId
+              name
+              statements {
+                index
+                property {
+                  name
+                  meta
+                  i18n {
+                    lang {
+                      code
+                    }
+                    text
+                  }
+                }
+                entity {
+                  value
+                  reference {
+                    itemId
+                  }
+                  meta
+                }
+              }
+              introduction {
+                id
+                index
+                markup {
+                  type
+                  start
+                  end
+                  href
+                  rel
+                  src
+                  title
+                }
+                text
+                type
+                meta
+              }
+              images {
+                id
+                name
+                file {
+                  path
+                }
+              }
+              meta
+            }
+          }
+        `,
+        variables: {
+          itemWhereInput: where,
+          itemOrderBy: orderBy
+        }
+      })
+
+      commit(types.ITEM_SEARCH_RESULTS, res.data.items)
+    } catch (error) {
+      throw new Error(error)
+    }
+  },
+
   setNowPage ({ commit }, data) {
     commit(types.ITEM_NOWPAGE, data)
+  },
+
+  setSearchMode ({ commit }, data) {
+    commit(types.ITEM_SEARCH_MODE, data)
+  },
+
+  setSearchRawText ({ commit }, data) {
+    commit(types.ITEM_SEARCH_RAW_TEXT, data)
+  },
+
+  setSearchOrderBy ({ commit }, data) {
+    commit(types.ITEM_SEARCH_ORDERBY, data)
+  },
+
+  setSearchResultMode ({ commit }, data) {
+    commit(types.ITEM_SEARCH_RESULT_MODE, data)
   }
 }
 
@@ -199,5 +296,27 @@ export const mutations = {
 
   [types.ITEM_NOWPAGE] (state, data) {
     state.nowPage = data
+  },
+
+  [types.ITEM_SEARCH_MODE] (state, data) {
+    state.search.mode === data
+      ? state.search.mode = 'NORMAL'
+      : state.search.mode = data
+  },
+
+  [types.ITEM_SEARCH_RESULTS] (state, data) {
+    state.search.result = data
+  },
+
+  [types.ITEM_SEARCH_RAW_TEXT] (state, data) {
+    state.search.rawText = data
+  },
+
+  [types.ITEM_SEARCH_ORDERBY] (state, data) {
+    state.search.orderBy = data
+  },
+
+  [types.ITEM_SEARCH_RESULT_MODE] (state, data) {
+    state.search.resultMode = data
   }
 }
